a71418212983517f40c492430e2ae7b538bd0450
Aliaksandr <MY_NAME@example.com>
2023-11-23 21:17:27 +0300

    add form control

--- main.js
+++ main.js
@@ -5,5 +5,6 @@ import 'reset-css'
 import './main.scss'
 
 // Import Components
-import './src/components/typography/typography.js'
-import './src/components/grid/grid.js'
+import './src/components/typography/typography.js';
+import './src/components/grid/grid.js';
+import './src/components/forms/forms.js';
