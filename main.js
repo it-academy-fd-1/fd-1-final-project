475ba96de6de31e922842c8723f040732e4f901b
Aliaksandr <MY_NAME@example.com>
2023-11-28 21:19:02 +0300

    add header

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ import 'reset-css'
 import './main.scss'
 
 // Import Components
+
 import './src/components/typography/typography.js';
 import './src/components/grid/grid.js';
 import './src/components/forms/forms.js';
+import './src/components/button/button.js';
+import './src/components/app-header/app-header.js'
+import './src/components/main-menu/main-menu.js'
+
+import './src/global/utils.scss';
--- src/components/forms/forms.js
+++ src/components/forms/forms.js
@@ -1,1 +1,3 @@
-import './inputs/form-control.scss'
+import './inputs/form-control.scss';
+import './inputs/input-group.scss';
+import './select/select.scss';
