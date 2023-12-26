56a00fb94e34d933d73045b5cbe0400307a7b014
Aliaksandr <MY_NAME@example.com>
2023-12-26 19:51:33 +0300

    init firebase

---
+++ configs/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: "fd-1-final-project-e0f43.firebaseapp.com",
+    projectId: "fd-1-final-project-e0f43",
+    storageBucket: "fd-1-final-project-e0f43.appspot.com",
+    messagingSenderId: "443087492012",
+    appId: "1:443087492012:web:a8d6aba4aaa63fdbbe2009"
+};
+
+export const app = initializeApp(firebaseConfig);
